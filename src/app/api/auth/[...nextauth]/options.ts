534ef20cb0aa7098e11b853d5bc0dd96e3fea02a
amin <chegini.amin@gmail.com>
2024-05-05 14:35:02 +0300

    update nextjs config

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -41,7 +41,7 @@ export const options: NextAuthOptions = {
       }),
    ],
    pages: {
-      signIn: '/auth/signin',
+      // signIn: '/auth/signin',
       // verifyRequest: '/auth/verify-request', // (used for check email message)
       // newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
    },
